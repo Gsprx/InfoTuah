01690c90d6d7b32ae0fd65349ddbc78838facb9a
Phlorion <gigadroll44thebest@gmail.com>
2024-11-25 14:16:26 +0200

    burger-menu

---
+++ navbar.js
@@ -0,0 +1,18 @@
+// get the dropdown navbar element
+const navbarDropdown = document.querySelector(".navbar-dropdown");
+
+// get burger menu button
+const burgerMenuButton = document.querySelector(".burger-menu-button");
+// add onClick event listener
+burgerMenuButton.addEventListener("click", function () {
+  // show the navbar dropdown
+  navbarDropdown.style.display = "block";
+});
+
+// get close button
+const closeMenuButton = document.querySelector(".close-menu-button");
+// add onClick event listener
+closeMenuButton.addEventListener("click", function () {
+  // hide the navbar dropdown
+  navbarDropdown.style.display = "none";
+});
